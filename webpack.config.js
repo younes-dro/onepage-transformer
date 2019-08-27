f7b55b15c813aedcd550ad6de9ac3290993e139a
younes-dro <dro66_8@hotmail.fr>
2019-08-27 03:19:27 +0100

    Formatting the code

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
           exclude: /node_modules/,
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env',
+            presets: [
+              '@babel/preset-env',
               '@babel/react',
               {
               'plugins': ['@babel/plugin-proposal-class-properties']
