c914f3a0175d9b8539f1196650d206924b67c9dc
younes-dro <dro66_8@hotmail.fr>
2019-08-27 05:10:13 +0100

    Add webpack loader : image-webpack-loader , file-loader , style-loader, css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,36 @@ module.exports = {
             }
           ]
         }
-        }
+      },
+      {
+    test:/\.css$/,
+    use:['style-loader','css-loader']
+},
+{
+  test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+  use: [
+    {
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+        outputPath: 'fonts/'
+      }
+    }
+  ]
+},
+{
+  test: /\.(gif|png|jpe?g|svg)$/i,
+  use: [
+    'file-loader',
+    {
+      loader: 'image-webpack-loader',
+      options: {
+        bypassOnDebug: true, // webpack@1.x
+        disable: true, // webpack@2.x and newer
+      },
+    },
+  ],
+}
     ]
   }
 };
